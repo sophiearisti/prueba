037419532028e3770804ce444bb7dd8b9606d597
Sophia Aristizabal <saristizabal1017@gmail.com>
2023-09-29 08:44:50 -0500

    se añadió la funcionalidad de la notificación y se añadió la funcionalidad de cambiar la foto de perfil

--- js/infoCuenta.js
+++ js/infoCuenta.js
@@ -1,13 +1,37 @@
-$ = function(id) {
-    return document.getElementById(id);
-  }
-  
-  var show = function(id) {
-    $(id).style.display ='block';
-    document.body.classList.add('overlayActive');
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('mas').addEventListener('click', function() {
+      document.getElementById('fileUpload').click();
+  });
+
+  document.getElementById('fileUpload').addEventListener('change', function() {
+      var fileName = this.value.split('\\').pop();
+      document.querySelector('output').textContent = fileName;
+  });
+});
+
+function show(id) {
+  document.getElementById(id).style.display = 'block';
+  document.body.classList.add('overlayActive');
 }
 
-var hide = function(id) {
-    $(id).style.display ='none';
-    document.body.classList.remove('overlayActive');
+function hide(id) {
+  document.getElementById(id).style.display = 'none';
+  document.body.classList.remove('overlayActive');
 }
+
+const input = document.getElementById("fileUpload");
+const imgElement = document.getElementById("foto");
+
+input.addEventListener("change", () => {
+    displayImages();
+});
+
+function displayImages() {
+    const file = input.files[0];  // Tomar el primer archivo seleccionado
+    if (file) {
+        const newSrc = URL.createObjectURL(file);  // Crear una URL temporal para el archivo
+        imgElement.setAttribute("src", newSrc);  // Cambiar el src de la imagen
+    }
+}
+
+
---
+++ js/registro.js
@@ -0,0 +1,94 @@
+$(document).ready(function() {
+   
+    function showAlert(){
+        $('.alert').addClass("show");
+        $('.alert').removeClass("hide");
+        $('.alert').addClass("showAlert");
+        setTimeout(function(){
+            $('.alert').removeClass("show");
+            $('.alert').addClass("hide");
+        },10000);
+    };
+
+    $('.close-btn').click(function(){
+        $('.alert').removeClass("show");
+        $('.alert').addClass("hide");
+    });
+
+
+    $('.camposLlenar').on('submit', function(event) {
+        event.preventDefault();  // This prevents the form from being submitted
+
+        var isValid = true;  // Assume the form is valid to start\\
+        const yearMinimo =2005;
+
+        var inputID = $('#ID').val();
+        var inputNombreusuario = $('#Nombreusuario').val();
+        var inputcontrasena = $('#contrasena').val();
+        var inputnombre = $('#nombre').val();
+        var inputapellido = $('#apellido').val();
+        var inputtelefono = $('#telefono').val();
+        var inputcorreo = $('#correo').val();
+        var regex = /@javeriana\.edu\.co$/;
+        var inputfecha= $('#fecha').val();
+        var year = parseInt(inputfecha.split('-')[0]);    // Split the date string at '-' and take the first part
+
+        //evaluar si es un numero
+        if(isNaN(inputID) || inputID.trim() === "") 
+        {
+            console.log("ID is not a number");
+            isValid = false;
+            $('.msg').text('El ID solo debe contener números');
+            showAlert();
+        }
+        else if(inputNombreusuario.trim() === "") {
+            console.log("El input está vacío");
+            isValid = false;
+            $('.msg').text('Usuario no puede estar vacío');
+            showAlert();
+        }
+        else if(inputcontrasena.trim() === "") {
+            console.log("El input está vacío");
+            isValid = false;
+            $('.msg').text('Contraseña no puede estar vacía');
+            showAlert();
+        }
+        else if(inputnombre.trim() === "") {
+            console.log("El input está vacío");
+            isValid = false;
+            $('.msg').text('Nombre no puede estar vacío');
+            showAlert();
+        }
+        else if(inputapellido.trim() === "") {
+            console.log("El input está vacío");
+            isValid = false;
+            $('.msg').text('Apellido no puede estar vacío');
+            showAlert();
+        }
+        else if(isNaN(inputtelefono) || inputtelefono.trim() === "") 
+        {
+            console.log("telefono is a number");
+            isValid = false;
+            $('.msg').text('Teléfono solo debe tener numeros');
+            showAlert();
+        }
+        else if(!regex.test(inputcorreo) || inputcorreo.trim() === "") {
+            console.log("The email doesnt end with @javeriana.edu.co");
+            isValid = false;
+            $('.msg').text('Correo no pertenerce a la universidad Javeriana');
+            showAlert();
+        }
+        else if(year > yearMinimo) 
+        {
+            console.log("menor de 18");
+            isValid = false;
+            $('.msg').text('Debe ser mayor de 18 años');
+            showAlert();
+        }
+
+        if(isValid) {
+            $(this).unbind('submit').submit(); // Desvincula el evento 'submit' y luego envía el formulario
+        }
+    });
+});
+
