d3070ad608306d7dc8a7c4770b71998e2a796c17
Sophia Aristizabal <saristizabal1017@gmail.com>
2023-09-29 23:30:34 -0500

    termine la pantalla de editar cuenta y la de menu usuario, solo falta promociones

--- js/infoCuenta.js
+++ js/infoCuenta.js
@@ -1,37 +1,141 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('mas').addEventListener('click', function() {
-      document.getElementById('fileUpload').click();
+$(document).ready(function() {
+  
+    function initializeSessionStorage() {
+        var campos = ['Nombreusuario', 'contrasena', 'nombre', 'apellido', 'telefono', 'correo'];
+        campos.forEach(function(campo) {
+            var inputElement = $('#' + campo);
+            if (!sessionStorage.getItem(campo)) {
+                sessionStorage.setItem(campo, inputElement.val());
+            }
+            else
+            {
+                campos.forEach(function(campo) {
+                    $('#' + campo).val(sessionStorage.getItem(campo));
+                });
+            }
+        });
+    }
+
+    initializeSessionStorage();
+
+    // Evento al hacer clic en el botón Cancelar
+    $('#cancelar').click(function() {
+        var campos = ['Nombreusuario', 'contrasena', 'nombre', 'apellido', 'telefono', 'correo'];
+        campos.forEach(function(campo) {
+            $('#' + campo).val(sessionStorage.getItem(campo));
+        });
+    });
+
+
+  function showAlert() {
+      $('.alert').addClass("show");
+      $('.alert').removeClass("hide");
+      $('.alert').addClass("showAlert");
+      $('#mas').off('click');  // Desactiva el evento click en #mas
+  }
+
+  function hideAlert() {
+      $('.alert').removeClass("show");
+      $('.alert').addClass("hide");
+      bindMasEvent();  // Reactiva el evento click en #mas
+  }
+
+  function showAlert2(){
+    $('.alert2').addClass("show");
+    $('.alert2').removeClass("hide");
+    $('.alert2').addClass("showAlert");
+    setTimeout(function(){
+        $('.alert2').removeClass("show");
+        $('.alert2').addClass("hide");
+    },10000);
+};
+
+    $('.close-btn2').click(function(){
+        $('.alert2').removeClass("show");
+        $('.alert2').addClass("hide");
+    });
+
+  function bindMasEvent() {
+      $('#mas').on('click', function() {
+          $('#fileUpload').trigger('click');
+      });
+  }
+
+  bindMasEvent(); // Inicialmente vincula el evento
+
+  $('#fileUpload').on('change', function() {
+      const file = this.files[0];
+      if (file) {
+          const newSrc = URL.createObjectURL(file);
+          $('#foto').attr('src', newSrc);
+      }
+      const fileName = $(this).val().split('\\').pop();
+      $('output').text(fileName);
   });
 
-  document.getElementById('fileUpload').addEventListener('change', function() {
-      var fileName = this.value.split('\\').pop();
-      document.querySelector('output').textContent = fileName;
+  $('#eliminar').on('click', function() {
+      showAlert();
   });
+
+  $('.close-btn').on('click', function() {
+      hideAlert();
+  });
+
+  // Añade eventos a los botones Sí y No de la alerta
+  $('.yes-btn, .no-btn').on('click', function() {
+      hideAlert();
+  });
+
+  $('.camposCambiar').off('submit').on('submit', function(event) {
+    event.preventDefault();  // Prevent form submission
+
+    var isValid = true;
+    var inputNombreusuario = $('#Nombreusuario').val().trim();
+    var inputcontrasena = $('#contrasena').val().trim();
+    var inputtelefono = $('#telefono').val().trim();
+
+    if(inputNombreusuario === "") {
+        isValid = false;
+        $('.msg2').text('Usuario no puede estar vacío');
+        showAlert2();
+    }
+    else if(inputtelefono === "" || isNaN(inputtelefono)) {
+        isValid = false;
+        if (inputtelefono === "") {
+            $('.msg2').text('Teléfono no puede estar vacío');
+        } else {
+            $('.msg2').text('Teléfono solo debe tener números');
+        }
+        showAlert2();
+    }
+
+    if(isValid) {
+
+        saveToSessionStorage();
+        $('.msg2').text('Cambios guardados satisfactoriamente');
+        showAlert2();
+        this.submit();  // Submit the form
+
+    }
+});
+
+function saveToSessionStorage() {
+    var campos = ['Nombreusuario', 'contrasena', 'nombre', 'apellido', 'telefono', 'correo'];
+    campos.forEach(function(campo) {
+        var inputElement = $('#' + campo);
+        sessionStorage.setItem(campo, inputElement.val());
+    });
+}
+
 });
 
 function show(id) {
-  document.getElementById(id).style.display = 'block';
-  document.body.classList.add('overlayActive');
-}
-
-function hide(id) {
-  document.getElementById(id).style.display = 'none';
-  document.body.classList.remove('overlayActive');
-}
-
-const input = document.getElementById("fileUpload");
-const imgElement = document.getElementById("foto");
-
-input.addEventListener("change", () => {
-    displayImages();
-});
-
-function displayImages() {
-    const file = input.files[0];  // Tomar el primer archivo seleccionado
-    if (file) {
-        const newSrc = URL.createObjectURL(file);  // Crear una URL temporal para el archivo
-        imgElement.setAttribute("src", newSrc);  // Cambiar el src de la imagen
-    }
-}
-
-
+    document.getElementById(id).style.display = 'block';
+    document.body.classList.add('overlayActive');
+  }
+  
+  function hide(id) {
+    document.getElementById(id).style.display = 'none';
+    document.body.classList.remove('overlayActive');
+  }
+  
--- js/menuCliente.js
+++ js/menuCliente.js
@@ -1,13 +1,95 @@
-$ = function(id) {
-    return document.getElementById(id);
-  }
-  
-  var show = function(id) {
-    $(id).style.display ='block';
-    document.body.classList.add('overlayActive');
+// Definición de funciones y variables
+var getElem = function(id) {
+  return document.getElementById(id);
+}
+
+var show = function(id) {
+  getElem(id).style.display = 'block';
+  document.body.classList.add('overlayActive');
 }
 
 var hide = function(id) {
-    $(id).style.display ='none';
-    document.body.classList.remove('overlayActive');
+  getElem(id).style.display = 'none';
+  document.body.classList.remove('overlayActive');
 }
+
+
+function generateProducts(numProducts, containerSelector, popupId) {
+  var container = $(containerSelector);
+
+  for (var i = 0; i < numProducts; i++) {
+      var productBlock = `
+          <div class="producto">
+              <div class="circuloproducto">
+                  <a href="#" class="productLink">
+                      <img class="fotominipRODUCTO" src="../imagenes/PRODUCTOIMG.png" alt="foto Producto">
+                  </a>
+              </div>
+              <p class="nombreminiProducto">
+                  torta de chocolate
+              </p>
+          </div>
+      `;
+
+      container.append(productBlock);
+  }
+
+  // Es importante hacer el evento click DESPUÉS de agregar los productos al contenedor
+  container.find(".productLink").on("click", function(event) {
+      event.preventDefault();
+      show(popupId);
+  });
+}
+
+function generateTiendas(numTiendas, containerSelector) {
+  var container = $(containerSelector);
+
+  for (var i = 0; i < numTiendas; i++) {
+      var tiendaBlock = `
+      <div class="minitienda">
+          <div class="circuloTIENDA">
+            <a  href="#" onclick="show('')">
+              <img class="fotominiTIENDA" src="../imagenes/LaCentral.png" alt="foto Tienda">
+            </a>
+          </div>
+          
+          <p class="nombreminiTIENDA">
+            La central
+          </p>
+      </div>
+      `;
+
+      container.append(tiendaBlock);
+  }
+
+
+}
+
+$(document).ready(function() {
+  // Para el primer bloque
+  generateProducts(10, ".scrollBoxProducto", 'popup');
+
+  generateTiendas(10, ".scrollBoxTienda");
+
+  $('#basura').on('click', function() {
+    $('#cantidadProducto').val('1');
+  });
+
+  $('#mas').on('click', function() {
+    var currentValue = parseInt($('#cantidadProducto').val(), 10) || 0; // Convertir el valor a entero
+    $('#cantidadProducto').val(currentValue + 1);
+  });
+
+  $('.Bspagar').on('click', function() {
+    var cantidad = $('#cantidadProducto').val();
+    if (isNaN(cantidad) || cantidad.trim() === "") { 
+        // isNaN verifica si no es un número; la segunda condición verifica si está vacío o sólo contiene espacios.
+        alert('Por favor, introduce un número válido en la cantidad.');
+        return false; // Evita que se ejecute cualquier otro comportamiento del botón.
+    }
+    
+    // Aquí puedes continuar con cualquier otra lógica que necesites.
+});
+
+  
+});
